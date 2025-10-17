6e711853d349d189c324b76f4d81ed4ccffcec93
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-17 23:09:14 +0200

    imported page-specific CSS files into respective components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './assets/pages/Home/Home'
+import Cart from './assets/pages/Cart/Cart'
+import PlaceOrder from './assets/pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        < Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>}/>
+      </Routes>
+      
     </div>
   )
 }
 
-export default App
+export default App  
