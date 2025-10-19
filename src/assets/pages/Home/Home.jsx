8329e92400a9a12de0538b650f99811972e4d706
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-19 11:51:13 +0200

    created Header component with JSX and linked CSS file

--- src/assets/pages/Home/Home.jsx
+++ src/assets/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../../components/Header/Header'
 
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Header/>
+    </div>
   )
 }
 
