4c84b13d2a1b227cfd17c4e6c40b8799de703508
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-19 12:30:57 +0200

    rendered menu items dynamically from assets.js using map

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -4,7 +4,24 @@ import { menu_list } from '../../assets/assets'
 
 const ExploreMenu = () => {
   return (
-    <div></div>
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore our menu</h1>
+        <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes
+          crafted with the finest ingredients nd culinary expertise. Our mission
+          is to satisfy yours cravings and elevate your dining experience, one
+          delicious meal at a time.</p>
+        <div className="explore-menu-list">
+            {menu_list.map((item,index)=>{
+                return (
+                    <div key={index} className='explore-menu-list-item'>
+                        <img src={item.menu_image} alt="" />
+                        <p>{item.menu_name}</p>
+                    </div>
+                    
+                )
+            })}
+        </div>
+    </div>
   )
 }
 
