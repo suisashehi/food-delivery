5bd8f77d1925ddfac486fbdca1131270df135c9e
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-21 22:58:49 +0200

    added Add button and counter logic to manage quantity in Food Item component

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 
 const FoodItem = ({ id, name, price, description, image }) => {
+
+    const [itemCount,setItemCount] = useState(0)
+
   return (
     <div className="food-item">
       <div className="food-item-img-container">
