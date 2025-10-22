c4f696bf714e08fd27da065d4f67d6f875e3bdaa
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-22 23:43:25 +0200

    added category-based filtering in Food Display component for dynamic menu rendering

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -9,7 +9,10 @@ const FoodDisplay = ({ category }) => {
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list.map((item, index) => {
+        {food_list.map((item, index) => { 
+          if (category==="All" || category===item.category) {
+
+          
           return (
             <FoodItem
               key={index}
@@ -20,6 +23,7 @@ const FoodDisplay = ({ category }) => {
               image={item.image}
             />
           );
+          }
         })}
       </div>
     </div>
