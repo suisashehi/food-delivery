7229561e2a5ea7c3d2d6b0de7414632ec631472e
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-22 22:49:39 +0200

    removed local counter logic and migrated to centralized context-driven state

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -3,26 +3,28 @@ import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 
 const FoodItem = ({ id, name, price, description, image }) => {
-  const [itemCount, setItemCount] = useState(0);
+  const [itemCount, setItemCount] = useState(0)
+  const {cartItems, addToCart, removeFromCart} = useContext(StoreContext);
 
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img className="food-item-image" src={image} alt="" />
-        {!itemCount ? (
+        {!cartItems[id] ? (
           <img
             className="add"
-            onClick={() => setItemCount((prev) => prev + 1)}
+            onClick={() => addToCart(id)}
             src={assets.add_icon_white}
             alt=""
           />
         ) : (
           <div className="item-item-counter">
-            <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
-            <p>{itemCount}</p>
-            <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+            <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+            <p>{cartItems[id]}</p>
+            <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
+
           </div>
-        )}
+        )} 
       </div>
 
       <div className="food-item-info">
