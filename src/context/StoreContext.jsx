30ab4368ce136d8d59bae23f3427ec47dd937c3c
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-22 22:27:24 +0200

    initialized global cart state with Context API and implemented add/remove cart functions

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -3,6 +3,17 @@ import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = (itemId)=>{
+        if(!cartItems[itemId]){
+            setCartItems((prev)=>({...prev, [itemId]:1}))
+    }
+        else{setCartItems((prev)=>({...prev, [itemId]:prev[itemId]+1}))}
+}
+    const removeFromCart = (itemId)=>{
+        
+    }
 
 
 
