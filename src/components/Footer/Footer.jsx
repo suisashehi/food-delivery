5f41f93052bcd97fa5623a0abbaefa3b4e5cef15
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-24 23:20:10 +0200

    Populate footer content: left (logo, text, social icons), center (company links), right (contact info)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
   return (
-    <div className='footer' id='footer'>
-        <div className="footer-content">
-            <div className="footer-content-left">
-
-            </div>
-            <div className="footer-content-center">
-                
-            </div>
-            <div className="footer-content-right">
-
-            </div>
-            
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon}  alt="" />
+          </div>
         </div>
-
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+1-212-423-3121</li>
+                <li>contact@tomato.com</li>
+            </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
