ec634c9084a6fa9a9fbe989abedabcc3c16c51ca
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-29 21:41:01 +0100

    implemented AppDownload component layout with Play Store and App Store icons

--- src/assets/pages/Home/Home.jsx
+++ src/assets/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css'
 import Header from '../../../components/Header/Header'
 import ExploreMenu from '../../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../../components/FoodDisplay/FoodDisplay'
+import App from '../../../App'
+import AppDownload from '../../../components/AppDownload/AppDownload'
 
 const Home = () => {
 
@@ -13,6 +15,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
+      <AppDownload/>
     </div>
   )
 }
