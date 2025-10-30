725947c244adcefb42a18f8b4de081456f575acf
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-30 15:49:35 +0100

    passed popup toggle handler to Navbar

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./LoginPopup.css"
-const LoginPopup = () => {
+import { assets } from '../../assets/assets'
+const LoginPopup = ({setShowLogin}) => {
+
+    const [currState,setCurrState] = useState("Sign Up")
+
   return (
     <div className='login-popup'>
 
-
+        <form className="login-popup-container">
+            <div className="login-popup-title">
+                <h2>{currState}</h2>
+                <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+            </div>
+        </form>
 
     </div>
   )
