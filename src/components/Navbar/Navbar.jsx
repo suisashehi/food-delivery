feead77fb287d962ea317f9a09bf35cc029efad3
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-30 15:29:05 +0100

    initialize LoginPopup component and link with App.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
   const [menu, setMenu] = useState("home");
 
   return (
