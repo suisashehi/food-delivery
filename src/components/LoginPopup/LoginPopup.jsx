30519f24736996d9e41a102491e3b01eea548dd5
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-30 16:01:25 +0100

    added form skeleton and close functionality in LoginPopup

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -13,6 +13,13 @@ const LoginPopup = ({setShowLogin}) => {
                 <h2>{currState}</h2>
                 <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
             </div>
+            <div className="login-popup-inputs">
+                <input type="text" placeholder='Name' required />
+                <input type="email" placeholder='Email' required />
+                <input type="password" placeholder='Password' required />
+
+            </div>
+            <button>{currState==="Sign Up"?"Create account":"Login"}</button>
         </form>
 
     </div>
