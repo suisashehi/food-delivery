c2e746b580bc58f494a39e82f223015503c70487
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-31 18:46:22 +0100

    created table header for cart items

--- src/assets/pages/Cart/Cart.jsx
+++ src/assets/pages/Cart/Cart.jsx
@@ -18,6 +18,15 @@ const Cart = () => {
         </div>
         <br />
         <hr />
+        {food_list.map((item,index)=>{
+          if(cartItems[item._id]>0){
+            return(
+              <div className='cart-items-title cart-items-item'>
+               
+              </div>
+            )
+          }
+        })}
       </div>
     </div>
   );
