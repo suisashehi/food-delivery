bca85aab0aaa9276836a4bc4bb2b406b1cbe2b6d
Suisa Shehi <suisa_sh20@icloud.com>
2025-11-03 19:27:18 +0100

    setup navigation from cart to order page using useNavigate and initialized PlaceOrder page structure with left and right sections

--- src/assets/pages/Cart/Cart.jsx
+++ src/assets/pages/Cart/Cart.jsx
@@ -1,9 +1,12 @@
 import React, { useContext } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../../context/StoreContext";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { cartItems, food_list, removeFromCart, getTotalCartAmout } = useContext(StoreContext);
+
+  const navigate = useNavigate();
 
   return (
     <div className="cart">
@@ -57,7 +60,7 @@ const Cart = () => {
               <b>${getTotalCartAmout()+2}</b>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button> 
+          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button> 
         </div>
         <div className="cart-promocode">
           <div>
--- src/assets/pages/PlaceOrder/PlaceOrder.jsx
+++ src/assets/pages/PlaceOrder/PlaceOrder.jsx
@@ -3,7 +3,24 @@ import './PlaceOrder.css'
 
 const PlaceOrder = () => {
   return (
-    <div>PlaceOrder</div>
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className='title'>Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='First name' />
+          <input type="text" placeholder='Last name'/>
+        </div>
+        <input type="email" placeholder='Email address' />
+        <input type="text" placeholder='Street'/>
+        <div className="multi-fields">
+          <input type="text" placeholder='First name' />
+          <input type="text" placeholder='Last name'/>
+        </div>
+      </div>
+      <div className="place-order-right">
+
+      </div>
+    </form>
   )
 }
 
